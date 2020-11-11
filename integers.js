320c2417e4dce0e17f06901b9b44b4b5bbd6de1b
Brett Stoudt <Bstoudt01@gmail.com>
2020-11-11 15:07:28 -0600

    completed a palindrome problem to verify a number is a palindrome or not

--- integers.js
+++ integers.js
@@ -2,7 +2,7 @@
 //Given a 32-bit signed integer, reverse digits of an integer.
 //..storage environment can only handle 32-bit ints
 //remove trailing 0's and transpose negative signs when applicable
-let number = -123;
+let numberSeven = -123;
 
 var reverse = function (x) {
 
@@ -20,6 +20,58 @@ var reverse = function (x) {
     return y * Math.sign(x)
 };
 
-console.log("reserve", number, "is now", reverse(number));
+console.log("reserve", numberSeven, "is now", reverse(numberSeven));
+
+/****************************************************/
+
+
+//9. Palindrome Number
+//Determine whether an integer is a palindrome. 
+//..An integer is a palindrome when it reads the same backward as forward.
+//Follow up: Could you solve it without converting the integer to a string?
+
+const numberNine = 13231
+
+var isPalindrome = function (x) {
+
+    //hold onto the original number as it will be manipulated
+    let originalX = x
+
+    // 
+    var digit, result = 0
+
+    // if the number is zero ,return true / palindrome
+    if (Math.sign(x) == 0) {
+        return true
+    }
+
+    // if the number is a negative, return false / not a palindrome
+    if (Math.sign(x) == -1) {
+        return false
+    }
+
+    //
+    while (x) {
+
+        //  Get right-most digit. Ex. 123/10 → 12.3 → 3
+        digit = x % 10
+
+        //  set digit into result as the tail end of the number being created 
+        //...Ex. 123 → 1230 + 4 → 1234
+        result = (result * 10) + digit
+
+        // using OR expression to remove the trailing remainder  
+        //  Remove right-most digit. Ex. 123 → 12.3 → 12
+        x = x / 10 | 0
+        //x= Math.round(x/10|0) // This also seems to work but Math.round is not truly needed
+
+        if (result == originalX) {
+            return true
+        }
+    }
+
+    return false
+};
+console.log("is", numberNine, "a palindrome?", isPalindrome(numberNine));
 
 /****************************************************/
