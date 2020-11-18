d8a8b140ae7c21ebc5df60bad239571d91fab7dc
Brett Stoudt <Bstoudt01@gmail.com>
2020-11-18 11:27:18 -0600

    morning algorithms, compelted linked list shift, unshift, and get(byIndex), added comments on how we weere watching the head, and manipulating it

--- linkedList.js
+++ linkedList.js
@@ -158,6 +158,84 @@ class SinglyLinkedList {
             current = current.next
         }
     }
+
+    //shift, removing the node from the head of the list and return that node
+    shift() {
+        if (!this.head) return undefined;
+
+        //store the current head in a variable
+        let currentHead = this.head;
+
+        //set the head to be the current heads next node
+        this.head = currentHead.next;
+
+        //decrement length
+        this.length--;
+
+        //remove the tail, not required b.c it will be overwritten when the head is assigned
+        if (this.length === 0) {
+            this.tail = null;
+        }
+
+        //Return value of removed node
+        return currentHead;
+    }
+
+    unshift(val) {
+        //declare variable to hold new head values
+        let newHead = new Node(val);
+
+        //if there is no head, set the head and tail to our newHead
+        if (!this.head) {
+            this.head = newHead;
+            this.tail = newHead;
+            //this.tail = this.head
+        }
+        else {
+            //set the new nodes next property to the current head
+            //makes our connection to the linked list 
+            newHead.next = this.head;
+
+            //set the head to the new node
+            //assigned our newHead to the head of the list
+            this.head = newHead;
+        }
+        //Increment Length
+        this.length++;
+
+        //return the list.. or the head , or nothing.. etc..
+        return this;
+
+    }
+
+    //get - retrieve a node by its position
+    get(idx) {
+        //get .next n times, manually counting position (not like an array index)
+
+        //if idx is less than zero OR greater than or equal to length of the list, return null
+        if (idx < 0 || idx >= this.length) return null;
+
+
+        let currentNode = this.head;
+        //loop through the list until you reach the index
+        for (let i = 0; i <= idx; i++) {
+            //assign the currentNode varaible equal to the next property of that node
+            currentNode = currentNode.next;
+        }
+
+        //answer from algos
+        // let counter = 0;
+        // while(currentNode !== index) 
+        // {
+        //     currentNode = currentNode.next;
+        //     counter++;
+        // }
+
+
+        //return the node of that index (zero position index)
+        return currentNode;
+    }
+
 }
 
 //if tail is not set, use while loop to look at .next value until it is not equal to null
@@ -171,12 +249,14 @@ class SinglyLinkedList {
 var list = new SinglyLinkedList()
 list.push(10)
 list.push(11)
-
 list.push(12)
 list.push(99)
-console.log(list)
+//console.log(list)
 //console.log(list.pop());
-list.pop();
+// list.pop();
+//console.log(list.shift());
+//console.log(list.unshift(1))
 console.log(list)
 
-list.traverse();
+//console.log(list.get(2))
+// list.traverse();
