ff97f302227fa7c53dcdd2c1cbb083f37b741e4c
Brett Stoudt <Bstoudt01@gmail.com>
2020-12-03 16:16:40 -0600

    completed binary search algos today. was challenging to grasp the idea of only using the pointers for reference in our while loop and how that was actually working and how you need to handle out of bounds searches

--- search.js
+++ search.js
@@ -21,19 +21,44 @@ console.log(linearSearch([1, 2, 3, 4, 5], 4))
 //accepts sorted array and a vale (numbers for now)
 function binarySearch(array, value) {
     //index of left pointer / 0
-    let left = 0
+    let start = 0
     //index of right pointer / array.length - 1
-    let right = array.length - 1
-    //loop
-    for (let i = 0; i < array.length; i++) {
-        //found element?
-        //while left pointer comes before right pointer
-        //index of middle value 
-        //if value to small  left pointer up
-        //value to to large, move right pointer down
+    let end = array.length - 1
+
+    //round to down(floor) nearest whole number to get a middle index
+    let middle = Math.floor((start + end) / 2)
+
+
+    //condtionals if value is larger or smaller than ends of array or length is 0
+    //return -1
+    if (value < array[start] || value > array[end] || array.length === 0) return -1
+
+    //loop while value is not equal to our middle pointer
+    //and while starting pointer is less than end pointer
+    while (array[middle] !== value && start <= end) {
+
+
+        if (value < array[middle]) {
+            //if value is smaller than array middle value, then move the end pointer down 1 into the array
+            end = middle - 1
+        }
+        else {
+            //if the valuer is larger than array middle value, then move the start pointer up 1 further into the array
+            start = middle + 1
+        }
+        //reset middle based don new end or start
+        middle = Math.floor((end + start) / 2)
+
+    }
+
+    //if value = index value return it
+    if (array[middle] === value) {
+        return middle
     }
     //value doesnt exist? return -1
     return -1;
 
 
+    //    }
 }
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 50))
